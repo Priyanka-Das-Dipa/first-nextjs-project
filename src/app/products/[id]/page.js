847b06e5098582c7a31103321d7cfc051b474881
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-12-03 02:35:28 +0600

    first commit

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DynamicPage = ({params, searchParams}) => {
+    console.log(searchParams);
+    return (
+        <div>
+            This is Product dynamic page <strong>{params.id}</strong>  !!!
+            This is Search Params <strong>{searchParams.category}</strong>  !!!
+            
+        </div>
+    );
+};
+
+export default DynamicPage;
